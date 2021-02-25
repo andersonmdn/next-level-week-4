69fb729babac531803105666e563ea188a9d6a65
andersonmdn <andersonmdn0@hotmail.com>
2021-02-25 10:20:58 -0300

    Day 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,12 @@
 
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 import { ThemeSwitch } from './ThemeSwitch'
 
 export function Profile() {
+	const { level } = useContext(ChallengeContext);
+
 	return (
 		<div className={styles.profileContainer}>
 			<img src="https://github.com/andersonmdn.png" alt="Anderson André"/>
@@ -10,7 +14,7 @@ export function Profile() {
 				<strong>Anderson André</strong>
 				<p>
 					<img src="icons/level.svg" alt="Level"/>
-					Level 1
+					Level { level }
 				</p>
 			</div>
 			<ThemeSwitch />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import { Profile } from "../components/Profile";
 
 
 import styles from '../styles/pages/Home.module.css';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 export default function Home() {
   return (
@@ -18,17 +19,19 @@ export default function Home() {
 		</Head>
 		<ExperienceBar />
 
-		<section>
-			<div>
-				<Profile />
-				<CompletedChallenges />
-				<Countdown />
-			</div>
+		<CountdownProvider>
+			<section>	
+				<div>
+					<Profile />
+					<CompletedChallenges />
+					<Countdown />
+				</div>
 
-			<div>
-				<ChallengeBox />
-			</div>
-		</section>
+				<div>
+					<ChallengeBox />
+				</div>
+			</section>
+		</CountdownProvider>
 	</div>
   )
 }
